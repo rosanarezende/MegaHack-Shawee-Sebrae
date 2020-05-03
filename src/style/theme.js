f02b13805e2cf149cf623b8a30952cb20ee88885
Christhopher Porpino <cgporpinof@gmail.com>
2020-05-03 01:33:53 -0300

    ajustando cor do botão de login

--- src/style/theme.js
+++ src/style/theme.js
@@ -1,11 +1,12 @@
 import { createMuiTheme } from "@material-ui/core";
-import orange from "@material-ui/core/colors/orange";
 import red from '@material-ui/core/colors/red';
 import { pink } from "@material-ui/core/colors";
 
 export default createMuiTheme({
   palette: {
-    primary: pink,
+    primary: {
+      main: '#D12B63'
+    },
     secondary: red,
   }
 });
