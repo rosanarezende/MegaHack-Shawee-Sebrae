7bcdfbab398f04322c25b7ede4137879ab930599
Rosana Rezende <rezende_rosana@hotmail.com>
2020-05-03 00:18:53 -0300

    começando

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,14 +1,36 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
 import MyBottonNav from '../../components/BottonNav'
 
-function Home() {
+import { Typography, CardMedia } from '@material-ui/core'
 
+function Home(props) {
+  const { professionalData } = props
   return (
     <>
-      <p>Home</p>
+      <Typography align='center' variant='h3' gutterBottom color="primary">
+          {professionalData.businessName}
+      </Typography>
+
+      <CardMedia
+        image={professionalData.businessImage}
+        title={professionalData.businessName}
+      />
+
+
+
       <MyBottonNav />
     </>
   )
 }
 
-export default Home
+const mapStateToProps = state => ({
+  professionalData: state.professional.professionalData
+})
+
+// const mapDispatchToProps = dispatch => ({
+
+// })
+
+export default connect(mapStateToProps)(Home)
