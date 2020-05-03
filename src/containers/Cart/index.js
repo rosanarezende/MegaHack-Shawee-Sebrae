4b080fb6cd763f11273def0a3a7ab1be909d0b95
Rosana Rezende <rezende_rosana@hotmail.com>
2020-05-03 16:02:42 -0300

    carrinho com mocks fakes demais

--- src/containers/Cart/index.js
+++ src/containers/Cart/index.js
@@ -1,22 +1,165 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 
 import * as S from './styles'
 
 import MyBottonNav from '../../components/BottonNav'
+import { Card, CardHeader, Avatar, Typography } from '@material-ui/core'
+import ProductInCart from '../../components/ProductInCart'
 
-function Cart(props){
+function Cart(props) {
+    const { user } = props
+
+    const products = [
+        {
+            id: 1,
+            name: 'Sampoo Herbal',
+            value: 20.5,
+            img: 'https://user-images.githubusercontent.com/45580434/80917398-00894280-8d35-11ea-9892-beedc8447fc2.jpg',
+            date: 1588448549809,
+        },
+        {
+            id: 2,
+            name: 'Esmalte',
+            value: 12.95,
+            img: 'https://user-images.githubusercontent.com/45580434/80917325-6e813a00-8d34-11ea-9879-9bc0390698e1.jpg',
+            date: 1588448549809,
+        }
+    ]
+
+    const productsTotal = products.reduce((prevVal, product) => { return prevVal + product.value }, 0)
+
+    const frete = 5
+
+    const cashBack = 12.5
+
+    const [payment, setpayment] = useState('')
+
+    function savePaymentMethod(e) {
+        setpayment(e.target.id)
+    }
+
+    function onPlaceOrder(e) {
+        e.preventDefault()
+        // const placeOrderData = {
+        //     products: infoQuantity,
+        //     paymentMethod: payment
+        // }
+        // if (order && Object.keys(order).length !== 0) {
+        //     alert('Não é possível realizar dois pedidos ao mesmo tempo. Aguarde seu seu pedido ativo ser concluído!')
+        //     goToFeed()
+        // }
+        // else if (infoQuantity.length === 0) {
+        //     alert('Adicione produtos ao carrinho para realizar um pedido!')
+        // }
+        // else {
+        //     placeOrder(placeOrderData, restaurantId)
+        // }
+    }
 
     return (
         <>
             <S.CartWrapper>
-                Carrinho
+
+                <Card>
+                    <CardHeader
+                        avatar={
+                            <Avatar
+                                src={user.photo}
+                                alt={user.name}
+                            />
+                        }
+                        title={user.name}
+                        subheader={user.address}
+                    />
+                </Card>
+
+                <S.ProducstTitle gutterBottom variant="body1">
+                    Produtos selecionados:
+                </S.ProducstTitle>
+
+                {products.map(product => (
+                    <ProductInCart key={product.id} product={product} />
+                ))}
+
+                <S.DivValue>
+                    <Typography align="right">
+                        Subtotal: <strong>{productsTotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</strong>
+                    </Typography>
+                    <Typography align="right" >
+                        Frete: <span style={{ color: '#CE3145' }}>+ {frete.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</span>
+                    </Typography>
+                    <Typography align="right">
+                        Cashback disponível: <span style={{ color: '#4B6BD6' }}>- {cashBack.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</span>
+                    </Typography>
+                    <S.DivTotal>
+                        <Typography>
+                            TOTAL
+                        </Typography>
+                        <Typography align="right">
+                            <strong>{(productsTotal + frete - cashBack).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</strong>
+                        </Typography>
+                    </S.DivTotal>
+                </S.DivValue>
+
+                <S.Title>
+                    Formas de pagamentos:
+                </S.Title>
+
+                <S.FormWrapper onSubmit={onPlaceOrder}>
+
+                    <S.PayementOptionBoxWrapper>
+                        <S.RadioButtonWrapper
+                            name="payementOption"
+                            type="radio"
+                            value={payment || ''}
+                            id='money'
+                            required
+                            onClick={savePaymentMethod}
+                            disabled={products.length === 0 ? true : false}
+                        />
+
+                        <S.Text> Dinheiro </S.Text>
+                    </S.PayementOptionBoxWrapper>
+
+                    <S.PayementOptionBoxWrapper>
+                        <S.RadioButtonWrapper
+                            name="payementOption"
+                            type="radio"
+                            value={payment || ''}
+                            id='creditcard'
+                            onClick={savePaymentMethod}
+                            disabled={products.length === 0 ? true : false}
+                        />
+
+                        <S.Text> Cartão de crédito </S.Text>
+                    </S.PayementOptionBoxWrapper>
+
+                    <S.ConfirmButtonWrapper
+                        marginTop={products.length === 0 ? '145px' : '19px'}
+                        color={products.length === 0 ? 'rgba(232, 34, 46, 0.5)' : '#e8222e'}
+                        type='onsubmit'
+                    >
+                        Confirmar
+                </S.ConfirmButtonWrapper>
+
+                </S.FormWrapper>
+
+
             </S.CartWrapper>
 
-            <MyBottonNav/>
-            
+            <MyBottonNav />
+
         </>
     )
 }
 
-export default connect()(Cart)
+const mapStateToProps = state => ({
+    user: state.user.user
+})
+
+// const mapDispatchToProps = dispatch => ({
+
+// })
+
+export default connect(mapStateToProps)(Cart)
--- src/containers/Cart/styles.js
+++ src/containers/Cart/styles.js
@@ -1,5 +1,65 @@
 import styled from 'styled-components'
+import { Typography, Button } from '@material-ui/core'
 
 export const CartWrapper = styled.div`
+    margin: 2rem 2rem 5rem;
+`
 
+export const ProducstTitle = styled(Typography)`
+    margin: 2rem 0 1rem;
+`
+
+export const DivValue = styled.div`
+    margin-bottom: 2rem;
+`
+
+export const DivSubTotal = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+
+export const DivTotal = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin: 1rem 0 0;
+`
+
+export const Title = styled(Typography)`
+    margin: 16px 0 8px;
+    border-bottom: 1px solid black;
+    font-size: 16px;
+    letter-spacing: -0.39px;
+`
+
+export const FormWrapper = styled.form`
+`
+
+export const Text = styled(Typography)`
+    font-size: 16px;
+    height: 16px;
+    letter-spacing: -0.4px;
+    margin-bottom: 8px;
+    color: ${props => props.red ? "#e8222e" : "#000000"};
+    text-align: ${props => props.right && "right"};
+    font-weight: ${props => props.bold && "bold"};
+`
+
+export const PayementOptionBoxWrapper = styled.div`
+    display: flex;
+    align-items: center;
+`
+
+export const RadioButtonWrapper = styled.input`
+    width: 24px;
+    height: 24px;
+    margin-right: 8px;
+    margin-bottom: 11px;
+`
+export const ConfirmButtonWrapper = styled(Button)`
+    width: 100%;
+    height: 42px;
+    border-radius: 2px;
+    background-color: ${props => props.color};
+    margin: ${props => props.marginTop} 0 16px 0;
+    /* margin-bottom: 64px; */
 `
