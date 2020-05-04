4a84bc8e93b86cbd4d7b2f4bd8464fcd1e98667e
Rosana Rezende <rezende_rosana@hotmail.com>
2020-05-04 13:27:31 -0300

    formato do calendário

--- src/containers/Calendar/styles.js
+++ src/containers/Calendar/styles.js
@@ -1,10 +1,10 @@
 import styled from 'styled-components'
 
 export const CalendarWrapper = styled.div`
-    margin: 2rem;
+    margin: 1rem 2rem;
 `
 
 export const EventCalendarWrapper = styled.div`
-    margin: 3rem auto;
+    margin: 1rem auto;
     max-width: 1200px;
 `
